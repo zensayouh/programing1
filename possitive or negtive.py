044df92a79de86b4758f525ac30c4f72b20a864f
zensayouh <83812570+zensayouh@users.noreply.github.com>
2021-05-15 04:50:54 -0400

    Create possitive or negtive.py
    this code tke number from user and the code print if the number ppossitive or negtive

---
+++ possitive or negtive.py
@@ -0,0 +1,9 @@
+while True:
+    i=input("enter the number to check: ")
+    if i== '#':
+        break
+        x=int(i)
+    elif  x>=0 :
+        print("possitive")
+    else :
+        print("negative")
